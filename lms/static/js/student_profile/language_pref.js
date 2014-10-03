03b132944f8267fd680e15bcb85ccd2452f01899
Diana Huang <dkh@edx.org>
2014-10-03 11:48:36 -0400

    New templates and JS for the language selector

---
+++ lms/static/js/student_profile/language_pref.js
@@ -0,0 +1,10 @@
+var edx = edx || {};
+
+(function($, _, Backbone, gettext) {
+    'use strict';
+
+    edx.student = edx.student || {};
+
+    edx.student.profile.LanguagePreferenceModel = Backbone.Model.extend({
+    });
+})(jQuery, _, Backbone, gettext);
