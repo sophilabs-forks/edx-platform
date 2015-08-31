24d1a4ca766a1461e0377f6adaa454ddcc6e5551
TJ Keemon <keems21@gmail.com>
2015-08-31 14:16:37 -0400

    update model to handle course field

--- common/djangoapps/course_access_group/models.py
+++ common/djangoapps/course_access_group/models.py
@@ -5,12 +5,22 @@ from django.contrib.auth.models import User
 
 from xmodule_django.models import CourseKeyField
 
+#come up with better way of handling this
+#one to one relationship with Courses in mongodb
+class CourseStub(models.Model):
+	name = models.CharField(default='', max_length=255)
+	#course_id = models.CharField(default='', max_length=255, unique=True)
+	course_id = CourseKeyField(default='', max_length=255, unique=True)
+
+	def __str__(self):
+		return self.course_id
+
 class CourseAccessGroup(models.Model):
 	name = models.CharField(default='', max_length=32)
 	#figure out whether to use UserProfile or User object
 	students = models.ManyToManyField(User)
-	#courses = models.ManyToManyField(CourseKeyField)
-	#courses = models.CharField(max_length=32)
+	courses = models.ManyToManyField(CourseStub, blank=True)
 
 	def __str__(self):
 		return self.name
+
