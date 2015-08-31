6649d26f7760e01e11e77380a9befce8b7cf18e3
TJ Keemon <keems21@gmail.com>
2015-08-31 13:15:54 -0400

    fix name field in CourseAccessGroup

--- common/djangoapps/course_access_group/models.py
+++ common/djangoapps/course_access_group/models.py
@@ -6,9 +6,11 @@ from django.contrib.auth.models import User
 from xmodule_django.models import CourseKeyField
 
 class CourseAccessGroup(models.Model):
-	name = models.CharField(unique=True, max_length=32)
+	name = models.CharField(default='', max_length=32)
 	#figure out whether to use UserProfile or User object
 	students = models.ManyToManyField(User)
 	#courses = models.ManyToManyField(CourseKeyField)
 	#courses = models.CharField(max_length=32)
 
+	def __str__(self):
+		return self.name
