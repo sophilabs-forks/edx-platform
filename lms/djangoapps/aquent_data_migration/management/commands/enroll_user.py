984c8fa2e42ddbd0d1fb476e5d76f8bb97ea377a
TJ Keemon <keems21@gmail.com>
2015-09-16 16:17:19 -0400

    working management.py command for importing single user

--- lms/djangoapps/aquent_data_migration/management/commands/enroll_user.py
+++ lms/djangoapps/aquent_data_migration/management/commands/enroll_user.py
@@ -2,8 +2,13 @@
 """
 django management command: create single user based on CSV string
 """
+#os for devstack/testing only
+import os
 
 from django.core.management.base import BaseCommand
+
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
+from instructor.enrollment import enroll_email, get_user_email_language
 
 
 class Command(BaseCommand):
@@ -11,6 +16,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        print "args = ", args
+        ##### vars for testing
+        user_info = os.environ['AQ_USER'].split(',')
+        course_info = os.environ['AQ_COURSE'].split(',')
+        course_id = 'Gymnasium/100/0'
+        ##### end testing vars
 
+        #for user in users
+        #for course in courses
+        
+        #workflow from lms/djangoapps/instructor/views/api.py:students_update_enrollmen
+        #def students_update_enrollment(request, course_id):
+        course_id = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+        action = 'enroll'
+        # identifiers_raw = request.POST.get('identifiers')
+        # identifiers = _split_input_list(identifiers_raw)
+        auto_enroll = True
+        email_students = False
+
+
+        email = user_info[2] #skipping email validation
+        email_params = {}
+        language = get_user_email_language(None)
+
+        before, after = enroll_email(course_id, email, auto_enroll, email_students, email_params, language=language)
+        
+        print '---- user enrolled ----'
+        print before
+        print after
 
--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -5,3 +5,6 @@ from .appsembler import *
 
 INSTALLED_APPS += ('appsembler',)
 TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+
+#just temporary
+INSTALLED_APPS += ('aquent_data_migration',)
