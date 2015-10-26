039e0394b68ba9348e2823c0d7281ced29d5ffd4
Morgan Robertson <robertson.md7@gmail.com>
2015-10-26 13:42:17 -0400

    Don't set AWS_QUERYSTRING_AUTH.

--- cms/envs/docker_multi.py
+++ cms/envs/docker_multi.py
@@ -162,5 +162,4 @@ ELASTIC_SEARCH_CONFIG = ['elasticsearch:9200']
 # Settings for S3 static assets
 AWS_IS_GZIPPED = True
 AWS_HEADERS = {'Cache-Control': 'public, max-age=86400'}
-AWS_QUERYSTRING_AUTH = False
 AWS_STORAGE_BUCKET_PREFIX = EDX_PLATFORM_REVISION
