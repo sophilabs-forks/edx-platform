b6154acfa80ba3ee0172d265b92c475d216d6689
Morgan Robertson <robertson.md7@gmail.com>
2015-12-02 14:29:42 -0500

    Fix course discovery.

--- lms/envs/docker_multi.py
+++ lms/envs/docker_multi.py
@@ -2,3 +2,6 @@ from .docker import *
 
 # Override the default Elasticsearch configuration for edx-search
 ELASTIC_SEARCH_CONFIG = ['elasticsearch:9200']
+
+# Needed to enable course discovery
+SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
