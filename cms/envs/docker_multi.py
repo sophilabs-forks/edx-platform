50b690a06c2d42e60cca19b9a1dab9843911a174
Morgan Robertson <robertson.md7@gmail.com>
2015-12-02 14:28:44 -0500

    Remove unused S3 settings.

--- cms/envs/docker_multi.py
+++ cms/envs/docker_multi.py
@@ -2,8 +2,3 @@ from .docker import *
 
 # Override the default Elasticsearch configuration for edx-search
 ELASTIC_SEARCH_CONFIG = ['elasticsearch:9200']
-
-# Settings for S3 static assets
-AWS_IS_GZIPPED = True
-AWS_HEADERS = {'Cache-Control': 'public, max-age=86400'}
-AWS_STORAGE_BUCKET_PREFIX = EDX_PLATFORM_REVISION
--- lms/envs/docker_multi.py
+++ lms/envs/docker_multi.py
@@ -2,7 +2,3 @@ from .docker import *
 
 # Override the default Elasticsearch configuration for edx-search
 ELASTIC_SEARCH_CONFIG = ['elasticsearch:9200']
-
-# Settings for S3 static assets
-AWS_IS_GZIPPED = True
-AWS_HEADERS = {'Cache-Control': 'public, max-age=86400'}
