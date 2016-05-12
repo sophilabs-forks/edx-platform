7ed8e48e1d68eca36783e136092b0f4cd63a9c40
Filip Jukić <filip@jukic.me>
2016-05-12 10:37:38 +0200

    move auth backend setting to appsembler.py

--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -5,4 +5,3 @@ from .appsembler import *
 
 INSTALLED_APPS += ('appsembler',)
 DEFAULT_TEMPLATE_ENGINE['OPTIONS']['context_processors'] += ('appsembler.context_processors.intercom',)
-AUTHENTICATION_BACKENDS = ('organizations.backends.OrganizationMemberMicrositeBackend',)
