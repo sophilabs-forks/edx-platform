2f02fa9a8becb88e7287870e3e05cad4daf9f17a
Filip Jukić <filip@jukic.me>
2016-05-12 10:34:12 +0200

    install custom version of edx-organizations

--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -5,3 +5,4 @@ from .appsembler import *
 
 INSTALLED_APPS += ('appsembler',)
 DEFAULT_TEMPLATE_ENGINE['OPTIONS']['context_processors'] += ('appsembler.context_processors.intercom',)
+AUTHENTICATION_BACKENDS = ('organizations.backends.OrganizationMemberMicrositeBackend',)
