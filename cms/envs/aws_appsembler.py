0c38aad61e0e532ad1dca9dff96f32816f0fd1bd
TJ Keemon <keems21@gmail.com>
2016-05-18 09:01:19 +0800

    copy appsembler app refactor settings to cms

--- cms/envs/aws_appsembler.py
+++ cms/envs/aws_appsembler.py
@@ -3,8 +3,11 @@
 from .aws import *
 from .appsembler import *
 
-INSTALLED_APPS += ('appsembler',)
-TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+INSTALLED_APPS += (
+    'appsembler.intercom_integration',
+    'appsembler.enrollment',
+)
+TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.intercom_integration.context_processors.intercom',)
 
 SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
 
