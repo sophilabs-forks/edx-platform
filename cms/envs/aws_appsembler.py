73eec0201181b4f7847f42c89bb8f8245d3f8643
Filip Jukić <filip@jukic.me>
2016-06-28 09:27:55 +0200

    don't need the enrollment app in cms

--- cms/envs/aws_appsembler.py
+++ cms/envs/aws_appsembler.py
@@ -5,7 +5,6 @@ from .appsembler import *
 
 INSTALLED_APPS += (
     'appsembler.intercom_integration',
-    'appsembler.enrollment',
 )
 TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.intercom_integration.context_processors.intercom',)
 
