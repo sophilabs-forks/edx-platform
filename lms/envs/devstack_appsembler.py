32cb85ba091cf69392d7243d30cf0b5a6929c9e5
Filip Jukic <filip@jukic.me>
2016-07-18 13:52:42 +0200

    disable cache in devstack

--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -7,3 +7,9 @@ INSTALLED_APPS += ('appsembler',)
 DEFAULT_TEMPLATE_ENGINE['OPTIONS']['context_processors'] += ('appsembler.context_processors.intercom',)
 
 INSTALLED_APPS += ('hr_management',)
+
+# disable caching in dev environment
+for cache_key in CACHES.keys():
+    CACHES[cache_key]['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
