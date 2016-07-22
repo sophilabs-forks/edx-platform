b7e2b35e2c0a7efa674171f84736f450b6da1946
Filip Jukić <filip@jukic.me>
2016-07-22 12:00:05 +0200

    add sentry

--- cms/envs/amc.py
+++ cms/envs/amc.py
@@ -9,3 +9,15 @@ APPSEMBLER_SECRET_KEY = AUTH_TOKENS.get("APPSEMBLER_SECRET_KEY")
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = AUTH_TOKENS.get("MAILGUN_ACCESS_KEY")
 MAILGUN_SERVER_NAME = AUTH_TOKENS.get("MAILGUN_SERVER_NAME")
+
+
+# SENTRY
+SENTRY_DSN = AUTH_TOKENS.get('SENTRY_DSN', False)
+
+if SENTRY_DSN:
+    # Set your DSN value
+    RAVEN_CONFIG = {
+        'dsn': SENTRY_DSN,
+    }
+
+    INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
--- lms/envs/amc.py
+++ lms/envs/amc.py
@@ -14,3 +14,14 @@ MAILGUN_ACCESS_KEY = AUTH_TOKENS.get("MAILGUN_ACCESS_KEY")
 MAILGUN_SERVER_NAME = AUTH_TOKENS.get("MAILGUN_SERVER_NAME")
 
 INSTALLED_APPS += ('appsembler_lms',)
+
+# SENTRY
+SENTRY_DSN = AUTH_TOKENS.get('SENTRY_DSN', False)
+
+if SENTRY_DSN:
+    # Set your DSN value
+    RAVEN_CONFIG = {
+        'dsn': SENTRY_DSN,
+    }
+
+    INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
