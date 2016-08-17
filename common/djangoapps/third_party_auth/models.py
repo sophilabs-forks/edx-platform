a2e2d1020a792dc6da4941bfcf2acce1e4d21151
melvinsoft <melvinsoft@gmail.com>
2016-08-17 19:54:34 -0300

    add the ability to clean up usernames for SSO autoregister

--- common/djangoapps/third_party_auth/models.py
+++ common/djangoapps/third_party_auth/models.py
@@ -5,6 +5,9 @@ Models used to implement SAML SSO support in third_party_auth
 """
 from __future__ import absolute_import
 
+import re
+from random import randrange
+
 from config_models.models import ConfigurationModel, cache
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -194,6 +197,32 @@ class ProviderConfig(ConfigurationModel):
         # an error on submit.
         suggested_username = pipeline_kwargs.get('username')
 
+        # if the FEATURE flag ENABLE_THIRD_PARTY_AUTH_CLEAN_USERNAMES is set to
+        # True we clean all special chars from the username, this feature is
+        # configurable by this three env settings with this default:
+        # TPA_CLEAN_USERNAMES_KEEP_DOMAIN_PART: false
+        # TPA_CLEAN_USERNAMES_REPLACER_CHAR: ""
+        # TPA_CLEAN_USERNAMES_ADD_RANDOM_INT: false
+        # You can override this three in your settings.
+        if settings.FEATURES.get("ENABLE_THIRD_PARTY_AUTH_CLEAN_USERNAMES"):
+            if not settings.TPA_CLEAN_USERNAMES_KEEP_DOMAIN_PART:
+                if len(
+                    re.findall(r'[^@]+@[^@]+\.[^@]+', suggested_username)
+                    ) > 0:
+                    suggested_username = suggested_username.split('@')[0]
+
+            suggested_username = re.sub(
+                r'[^a-zA-Z0-9]',
+                settings.TPA_CLEAN_USERNAMES_REPLACER_CHAR,
+                suggested_username
+            )
+
+            if settings.TPA_CLEAN_USERNAMES_ADD_RANDOM_INT:
+                suggested_username = suggested_username[:27] if len(suggested_username) > 27 else suggested_username
+                suggested_username += str(randrange(100, 999))
+            else:
+                suggested_username = suggested_username[:30] if len(suggested_username) > 30 else suggested_username
+        #
         return {
             'email': details.get('email', ''),
             'name': details.get('fullname', ''),
