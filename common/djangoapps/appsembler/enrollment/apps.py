996012d27939300fa57c4d3e8f841a2adb1ff14c
melvinsoft <melvinsoft@gmail.com>
2016-08-29 17:58:46 -0300

    fix enrollment duplicated label app

---
+++ common/djangoapps/appsembler/enrollment/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class EnrollmentConfig(AppConfig):
+    #
+    name = 'appsembler.enrollment'
+    label = 'appsembler.enrollment'
+    verbose_name = 'Appsembler enrollment'
