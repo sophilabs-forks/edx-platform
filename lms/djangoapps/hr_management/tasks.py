2b82761f6a665edea6e39f87f2c3ba8057bd3061
TJ Keemon <keems21@gmail.com>
2016-09-22 18:08:00 +0800

    finally working report generation celery task

--- lms/djangoapps/hr_management/tasks.py
+++ lms/djangoapps/hr_management/tasks.py
@@ -3,7 +3,15 @@ from smtplib import SMTPException
 from celery.task import task
 from celery.utils.log import get_task_logger
 from django.core.mail import send_mail
+from datetime import date, datetime, timedelta
 
+from django.conf import settings
+from django.contrib.auth.models import User
+from student.models import CourseEnrollment
+from xmodule.modulestore.django import modulestore
+from organizations.models import Organization
+
+from .utils import generate_csv_grade_string
 
 LOGGER = get_task_logger(__name__)
 
@@ -18,3 +26,45 @@ def send_email_to_user(subject, message, from_address, email_addresses):
             LOGGER.error('Error sending email to {}'.format(email))
         else:
             LOGGER.debug('Successfully sent email to {}'.format(email))
+
+@task()
+def generate_and_email_microsite_report():
+    """ This should happen on the first of every month. """
+    pass
+
+@task()
+def generate_and_email_nyif_report():
+    """ 
+    This should happen on the first of every month. 
+    Generates a sitewide report
+    """
+
+    total_users = len(User.objects.all())
+    total_courses = len(modulestore().get_courses())
+    total_enrollments = len(CourseEnrollment.objects.all())
+    total_organizations = len(Organization.objects.all())
+
+    raw_grade_data = generate_csv_grade_string()
+    last_month = (date.today().replace(day=1) - timedelta(days=1)).strftime('%B')
+
+    email_subject = 'NYIF CCA Report for {}'.format(last_month)
+    email_content = """
+NYIF Corperate Client Report
+{date}
+
+Overview
+    Number of Users: {num_users}
+    Number of Courses: {num_courses}
+    Number of Enrollments: {num_enrollments}
+    Number of Organizations {num_organizations}
+
+Raw student grade data
+{grade_data}
+    """.format(date=datetime.now().date(),
+            num_users=total_users,
+            num_courses=total_courses,
+            num_enrollments=total_enrollments,
+            num_organizations=total_organizations,
+            grade_data=raw_grade_data
+        )
+    send_mail(email_subject, email_content, settings.DEFAULT_FROM_EMAIL, ['tj@appsembler.com'])
--- lms/djangoapps/hr_management/utils.py
+++ lms/djangoapps/hr_management/utils.py
@@ -1,5 +1,14 @@
 from hr_management.models import CourseAccessRequest
 
+from instructor.offline_gradecalc import student_grades
+from instructor.utils import DummyRequest
+from student.models import CourseEnrollment
+from courseware.courses import get_course_by_id
+from django.contrib.auth.models import User
+
+from datetime import datetime
+import csv
+import io
 
 def requested_access_for_course(course, user):
     """
@@ -11,3 +20,55 @@ def requested_access_for_course(course, user):
         return CourseAccessRequest.has_requested_access(user, course.id)
     else:
         return False
+
+def generate_csv_grade_string(organization=None):
+    """
+    Create a CSV string that will be included in weekly report email
+    """
+    header = ['#full_name','email','course_name','enrollment_date','progress','completion_date','score']
+    encoded_header = [unicode(s).encode('utf-8') for s in header ]
+    fp = io.BytesIO()
+    writer = csv.writer(fp, quotechar='"', quoting=csv.QUOTE_ALL)
+    writer.writerow(encoded_header)
+
+    if not organization:
+        student_list = User.objects.all()
+    else:
+        student_list = User.objects.filter(organizations__short_name=organization)
+
+    request = DummyRequest()
+
+    output_data = []
+    for student in student_list:
+        try:
+            full_name = student.profile.name
+        except: 
+            continue
+        email = student.email
+        for enrollment in student.courseenrollment_set.all(): 
+            try:
+                course = get_course_by_id(enrollment.course_id)
+            except: 
+                continue
+            grade = student_grades(student, request, course)
+            enrollment_date = enrollment.created
+            course_name = course.display_name
+            progress = ''
+            completion_date = ''
+            score = grade['percent']
+            row = [
+                full_name,
+                email,
+                course_name,
+                enrollment_date,
+                progress,
+                completion_date,
+                score
+            ]
+            encoded_row = [unicode(s).encode('utf-8') for s in row]
+            writer.writerow(encoded_row)
+
+    raw_grade_data = fp.getvalue()
+    fp.close()
+
+    return raw_grade_data
