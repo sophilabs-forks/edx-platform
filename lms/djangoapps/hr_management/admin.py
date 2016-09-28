732e85e5da86adeca06e6ed683d26154fa22caea
TJ Keemon <keems21@gmail.com>
2016-09-28 17:53:11 +0800

    add SitewideReportList to admin panel

--- lms/djangoapps/hr_management/admin.py
+++ lms/djangoapps/hr_management/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import HrManager, CourseAccessRequest, CourseCCASettings
+from .models import HrManager, CourseAccessRequest, CourseCCASettings, SitewideReportList
 
 admin.site.register(HrManager)
 admin.site.register(CourseAccessRequest)
 admin.site.register(CourseCCASettings)
+admin.site.register(SitewideReportList)
