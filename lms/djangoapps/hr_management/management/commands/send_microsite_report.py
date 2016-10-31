d8b2d80871f9a1b1c4f7313c5c416c6c83d9d6dc
TJ Keemon <keems21@gmail.com>
2016-10-31 16:29:14 +0800

    microsite report to management command

--- lms/djangoapps/hr_management/management/commands/send_microsite_report.py
+++ lms/djangoapps/hr_management/management/commands/send_microsite_report.py
@@ -1,19 +1,19 @@
 """
-Email NYIF sitewide report to all users on list 
+Email microsite report to specified hr managers 
 """
 from textwrap import dedent
 
 from django.core.management.base import BaseCommand
 
-from hr_management.tasks import generate_and_email_nyif_report
+from hr_management.tasks import generate_and_email_customer_report
 
 
 class Command(BaseCommand):
     """
-    Generate sitewide report and email to site admins
+    Generate microsite report and email to specified hr managers
     """
     args = ""
     help = dedent(__doc__).strip()
 
     def handle(self, *args, **options):
-        generate_and_email_nyif_report()
+        generate_and_email_customer_report()
