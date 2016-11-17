0653501284d34d561c97983d14658bd3e026bbd1
TJ Keemon <keems21@gmail.com>
2016-11-17 14:57:02 +0800

    remove old template context processor for now

--- cms/envs/aws_appsembler.py
+++ cms/envs/aws_appsembler.py
@@ -4,4 +4,4 @@ from .aws import *
 from .appsembler import *
 
 INSTALLED_APPS += ('appsembler',)
-TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+#TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
