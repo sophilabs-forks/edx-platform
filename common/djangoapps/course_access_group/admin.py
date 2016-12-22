97b79cf6cb76b8cf0a3537a6fd3bf80f8256a875
TJ Keemon <keems21@gmail.com>
2016-12-21 21:27:29 -0500

    make blacklist visible in /admin

--- common/djangoapps/course_access_group/admin.py
+++ common/djangoapps/course_access_group/admin.py
@@ -5,6 +5,7 @@ Admin site bindings for course_access_group
 from django.contrib import admin
 
 # from config_models.admin import ConfigurationModelAdmin
-from .models import CourseAccessGroup
+from .models import CourseAccessGroup, DomainBlacklist
 
 admin.site.register(CourseAccessGroup)
+admin.site.register(DomainBlacklist)
