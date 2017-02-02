b57fa4174983b768a420bab98f8f8cc694a41950
John Baldwin <jlbaldwin@gmail.com>
2017-02-02 10:47:44 -0500

    Search API for Taxoman integration: Updated search_api permissions to allow superusers w/out staff role to use the search_api

--- cms/djangoapps/search_api/permissions.py
+++ cms/djangoapps/search_api/permissions.py
@@ -6,4 +6,5 @@ class IsStaffUser(BasePermission):
     Allow access to only staff users
     """
     def has_permission(self, request, view):
-        return request.user and request.user.is_staff and request.user.is_active
+        return request.user and request.user.is_active and (
+            request.user.is_staff or request.user.is_superuser)
