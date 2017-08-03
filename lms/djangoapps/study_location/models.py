e3cde097c1aef9b26a5ebc9dea77d0f13afe77bc
Bryan Wilson <bryanlandia@gmail.com>
2017-08-03 11:26:26 -0700

    Remove created_date__lte filter on StudentStudyLocation was causing some issues on prod but not on staging, something to do with datetime.  Wasn't strictly necessary anyhow.

--- lms/djangoapps/study_location/models.py
+++ lms/djangoapps/study_location/models.py
@@ -65,7 +65,7 @@ class StudentStudyLocation(models.Model):
         Return the most recent study location prior to the date passed
         """
         try:
-            sloc = cls.objects.filter(user=student.id, created_date__lte=before_when).latest('created_date')
+            sloc = cls.objects.filter(user=student.id).latest('created_date')
         except cls.DoesNotExist:
             return None
         return sloc
