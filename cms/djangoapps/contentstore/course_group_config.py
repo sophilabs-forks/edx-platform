2e7268fe8f7add88ba1c07120deeffee353cea3c
Bryan Wilson <bryanlandia@gmail.com>
2017-08-25 16:19:47 -0700

    Handle imports from cms.djangoapps.contentstore to fix search re-indexing after course deletion from sysadmin dashboard

--- cms/djangoapps/contentstore/course_group_config.py
+++ cms/djangoapps/contentstore/course_group_config.py
@@ -7,7 +7,7 @@ import logging
 from util.db import generate_int_id, MYSQL_MAX_INT
 
 from django.utils.translation import ugettext as _
-from contentstore.utils import reverse_usage_url
+from cms.djangoapps.contentstore.utils import reverse_usage_url
 from xmodule.partitions.partitions import UserPartition
 from xmodule.split_test_module import get_split_user_partitions
 from openedx.core.djangoapps.course_groups.partition_scheme import get_cohorted_user_partition
