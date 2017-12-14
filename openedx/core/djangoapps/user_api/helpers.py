496fa7fda19489aeb6336ef5f4ec52dc24baa317
Bryan Wilson <bryanlandia@gmail.com>
2017-12-14 11:40:15 -0800

    Allow custom field order for Login, Reg forms via configuration helper

--- openedx/core/djangoapps/user_api/helpers.py
+++ openedx/core/djangoapps/user_api/helpers.py
@@ -143,7 +143,7 @@ class FormDescription(object):
         "options", "supplementalLink", "supplementalText"
     ]
 
-    def __init__(self, method, submit_url, prologue=u"", epilogue=u""):
+    def __init__(self, method, submit_url, prologue=u"", epilogue=u"", field_order=None):
         """Configure how the form should be submitted.
 
         Args:
@@ -151,6 +151,7 @@ class FormDescription(object):
             submit_url (unicode): The URL where the form should be submitted.
             prologue (unicode): HTML string with a prologue/intro for the form.
             epilogue (unicode): HTML string with an epilogue/conclustion for the form.
+            field_order (list): List specifying a field order by field name
 
         """
         self.method = method
@@ -159,6 +160,7 @@ class FormDescription(object):
         self.prologue = prologue
         self.epilogue = epilogue
         self._field_overrides = defaultdict(dict)
+        self.field_order = field_order
 
     def add_field(
             self, name, label=u"", field_type=u"text", default=u"",
@@ -325,10 +327,15 @@ class FormDescription(object):
         Returns:
             unicode
         """
+
+        # apply cutom ordering here so that custom reg form fields, 
+        # enterprise extension fields, etc. are orderable
+        ordered_fields = self.apply_field_order(self.field_order)
+
         return json.dumps({
             "method": self.method,
             "submit_url": self.submit_url,
-            "fields": self.fields,
+            "fields": ordered_fields,
             "prologue": self.prologue,
             "epilogue": self.epilogue
         }, cls=LocalizedJSONEncoder)
@@ -362,6 +369,41 @@ class FormDescription(object):
             for property_name, property_value in kwargs.iteritems()
             if property_name in self.OVERRIDE_FIELD_PROPERTIES
         })
+
+    def apply_field_order(self, field_order):
+        """Apply a custom field order, if set.
+        Return ordered field list, or 
+        return self.fields if no order set.
+
+        If field name in order list is not found in self.fields,
+        just don't include it.  Duplicate field names in 
+        field order are stripped
+
+        Arguments:
+            field_order (List): List specifying order of fields
+            (none)
+        """
+        unordered = self.fields
+        field_order_unique = []
+        if not field_order:
+            return unordered
+        else:
+            for f in field_order:
+                if f not in field_order_unique:
+                    field_order_unique.append(f)
+            names = [f['name'] for f in unordered]
+            o_index = []
+            for f in field_order_unique:
+                try:
+                    o_index.append(names.index(f))
+                except ValueError:
+                    # drop unmatched
+                    msg = "{} in custom form field order not found in field list".format(f)
+                    LOGGER.warning(msg)
+                    
+            ordered = [unordered[i] for i in o_index]
+
+        return ordered
 
 
 class LocalizedJSONEncoder(DjangoJSONEncoder):
