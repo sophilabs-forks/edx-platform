2ed385ad56d70bc843373c2cb9b7c3bb110db5e9
Omar Al-Ithawi <i@omardo.com>
2018-02-20 21:40:32 +0200

    Merge Appsembler and edX's migrations for third_party_auth

---
+++ common/djangoapps/third_party_auth/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('third_party_auth', '0007_samlconfiguration_slo_redirect_url'),
+        ('third_party_auth', '0012_auto_20170626_1135'),
+    ]
+
+    operations = [
+    ]
