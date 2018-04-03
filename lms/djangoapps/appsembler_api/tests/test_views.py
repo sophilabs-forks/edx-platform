64026460e3555c32f43cfe65d06d0606b2e8f9e0
TJ Keemon <keems21@gmail.com>
2018-04-03 17:39:36 +0800

    tests still broken

--- lms/djangoapps/appsembler_api/tests/test_views.py
+++ lms/djangoapps/appsembler_api/tests/test_views.py
@@ -19,6 +19,9 @@ from search.tests.test_course_discovery import DemoCourse
 from mock import patch
 from datetime import datetime
 from rest_framework.permissions import AllowAny
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+# from openedx.core.lib.api.authentication import OAuth2AuthenticationAllowInactiveUser
+
 
 from student.tests.factories import UserFactory, CourseEnrollmentFactory
 from xmodule.modulestore.tests.factories import CourseFactory
@@ -26,108 +29,32 @@ from certificates.models import GeneratedCertificate
 
 from appsembler_api.views import GetBatchEnrollmentDataView
 
-# @override_settings(SEARCH_ENGINE="search.tests.mock_search_engine.MockSearchEngine")
-@patch.object(GetBatchEnrollmentDataView, 'permission_classes', [AllowAny])
+# @patch('appsembler_api.views.GetBatchEnrollmentDataView.permission_classes', [AllowAny])
+# @patch('appsembler_api.views.GetBatchEnrollmentDataView.authentication_classes', [])
+@patch('appsembler_api.views.GetBatchEnrollmentDataView.authentication_classes', [])
+@patch('appsembler_api.views.GetBatchEnrollmentDataView.permission_classes', [AllowAny])
 class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCase):
     """
     Tests for the endpoint: /analytics/enrollment/batch
     """
 
-    # def setUp(self):
-    #     super(AnalyticsEnrollmentBatchViewTest, self).setUp()
-    #     DemoCourse.reset_count()
-    #     self.searcher.destroy()
-
-    #     self.courses = [
-    #         self.add_course("OrgA", "Find this one with the right parameter"),
-    #         self.add_course("OrgB", "Find this one with another parameter"),
-    #         self.add_course("OrgC", "Find this one somehow"),
-    #     ]
-
-    #     self.url = reverse('course-list')
-    #     self.staff_user = self.create_user(username='staff', is_staff=True)
-    #     self.honor_user = self.create_user(username='honor', is_staff=False)
-
-    # def add_course(self, org_code, short_description):
-    #     """
-    #     Add a course to both database and search.
-    #     Warning: A ton of gluing here! If this fails, double check both CourseListViewTestCase and MockSearchUrlTest.
-    #     """
-
-    #     search_course = DemoCourse.get({
-    #         "org": org_code,
-    #         "run": "2010",
-    #         "number": "DemoZ",
-    #         "id": "{org_code}/DemoZ/2010".format(org_code=org_code),
-    #         "content": {
-    #             "short_description": short_description,
-    #         },
-    #     })
-
-    #     DemoCourse.index(self.searcher, [search_course])
-
-    #     org, course, run = search_course['id'].split('/')
-
-    #     db_course = self.create_course(
-    #         org=org,
-    #         course=course,
-    #         run=run,
-    #         short_description=short_description,
-    #     )
-
-    #     return db_course
-
-    # def search_request(self, search_term=''):
-    #     res = self.client.get(reverse("course_list_search"), data={'search_term': search_term})
-    #     return res.status_code, json.loads(res.content)
-
-    # def test_search_api_alone(self):
-    #     """
-    #     Double check that search alone works fine.
-    #     """
-    #     res = self.client.post(reverse('course_discovery'))
-    #     data = json.loads(res.content)
-    #     self.assertNotEqual(data["results"], [])
-    #     self.assertNotIn('course-v1', unicode(self.courses[0].id))
-    #     self.assertContains(res, unicode(self.courses[0].id))
-    #     self.assertEqual(data["total"], 3)
-
-    # def test_course_api_alone(self):
-    #     """
-    #     Double check that search alone works fine.
-    #     """
-    #     self.setup_user(self.staff_user)
-    #     response = self.verify_response(expected_status_code=200, params={'username': self.staff_user.username})
-    #     data = json.loads(response.content)
-    #     self.assertNotEqual(data["results"], [])
-    #     self.assertEqual(data["pagination"]["count"], 3)
-    #     self.assertNotIn('course-v1', response.content)
-
-    # def test_list_all(self):
-    #     """ test searching using the url """
-    #     code, data = self.search_request()
-    #     self.assertEqual(200, code)
-    #     self.assertIn("results", data)
-    #     self.assertNotEqual(data["results"], [])
-    #     self.assertEqual(data["pagination"]["count"], 3)
-
-    # def test_list_all_with_search_term(self):
-    #     """ test searching using the url """
-    #     code, data = self.search_request(search_term='somehow')
-    #     self.assertEqual(200, code)
-    #     self.assertIn("results", data)
-    #     self.assertNotEqual(data["results"], [])
-    #     self.assertEqual(data["pagination"]["count"], 1)
-
-@patch.object(GetBatchEnrollmentDataView, 'permission_classes', [AllowAny])
-class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCase):
-    """
-    Tests for the endpoint: /analytics/enrollment/batch
-    """
+    USERNAME = "Bob"
+    EMAIL = "bob@example.com"
+    PASSWORD = "edx"
 
     def setUp(self):
         super(AnalyticsEnrollmentBatchViewTest, self).setUp()
         
+        self.view = GetBatchEnrollmentDataView.as_view()
+
+        self.staff = UserFactory.create(
+            username=self.USERNAME,
+            email=self.EMAIL,
+            password=self.PASSWORD,
+            is_staff=True,
+        )
+        self.request_factory = APIRequestFactory()
+
         self.course1 = CourseFactory()
         self.course2 = CourseFactory()
 
@@ -153,7 +80,14 @@ class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCa
         self.url = reverse('get_batch_user_data')
 
     def test_analytics_enrollment_endpoint_alone(self):
-        res = self.client.get(self.url)
-        print res
-        self.assertEqual(res.status_code,4)
-        self.assertEquel(GeneratedCertificate.objects.count(),4)
+        request = self.request_factory.get(self.url, content_type='application/json')
+        force_authenticate(request, user=self.staff)
+        res = self.view(request)
+        res.render()
+
+        # res = self.client.get(self.url)
+        print res.content
+        self.assertEqual(res.status_code, 200)
+        # self.assertEqual(res.status_code,4)
+        # self.assertEqual(len(self.certificates),4)
+
