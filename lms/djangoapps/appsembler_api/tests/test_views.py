6ae0c0876c6a4a221a4d6729f631eb6ae800628f
TJ Keemon <keems21@gmail.com>
2018-04-04 16:14:42 +0800

    first working test

--- lms/djangoapps/appsembler_api/tests/test_views.py
+++ lms/djangoapps/appsembler_api/tests/test_views.py
@@ -14,6 +14,7 @@ from django.test.utils import override_settings
 
 from search.tests.test_course_discovery import DemoCourse
 
+import ddt
 
 ### for this endpoint test
 from mock import patch
@@ -27,10 +28,11 @@ from student.tests.factories import UserFactory, CourseEnrollmentFactory
 from xmodule.modulestore.tests.factories import CourseFactory
 from certificates.models import GeneratedCertificate
 
-from appsembler_api.views import GetBatchEnrollmentDataView
+# from appsembler_api.views import GetBatchEnrollmentDataView
 
-# @patch('appsembler_api.views.GetBatchEnrollmentDataView.permission_classes', [AllowAny])
+# @patch.object(GetBatchEnrollmentDataView, 'permission_classes', [AllowAny])
 # @patch('appsembler_api.views.GetBatchEnrollmentDataView.authentication_classes', [])
+@ddt.ddt
 @patch('appsembler_api.views.GetBatchEnrollmentDataView.authentication_classes', [])
 @patch('appsembler_api.views.GetBatchEnrollmentDataView.permission_classes', [AllowAny])
 class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCase):
@@ -45,7 +47,7 @@ class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCa
     def setUp(self):
         super(AnalyticsEnrollmentBatchViewTest, self).setUp()
         
-        self.view = GetBatchEnrollmentDataView.as_view()
+        # self.view = GetBatchEnrollmentDataView.as_view()
 
         self.staff = UserFactory.create(
             username=self.USERNAME,
@@ -77,17 +79,20 @@ class AnalyticsEnrollmentBatchViewTest(CourseApiTestViewMixin, ModuleStoreTestCa
                                 for ce in self.enrollments
                         ]
 
-        self.url = reverse('get_batch_user_data')
+        self.url = reverse('get_batch_enrollment_data')
 
-    def test_analytics_enrollment_endpoint_alone(self):
-        request = self.request_factory.get(self.url, content_type='application/json')
-        force_authenticate(request, user=self.staff)
-        res = self.view(request)
-        res.render()
+    @ddt.unpack
+    @ddt.data(  {'course_str': 'course1', 'num_enrollments': 3},
+                {'course_str': 'course2', 'num_enrollments': 1},)
+    def test_analytics_enrollment_endpoint_with_only_course_id(self, course_str, num_enrollments):
 
-        # res = self.client.get(self.url)
-        print res.content
+        course = getattr(self, course_str)
+        course_id = str(course.id)
+
+        res = self.client.get(test_url + '?course_id={}'.format(course_id))
+
+        self.asserIn('enrollment', res.content)
         self.assertEqual(res.status_code, 200)
-        # self.assertEqual(res.status_code,4)
-        # self.assertEqual(len(self.certificates),4)
 
+        data = res.data
+        self.assertEqual(len(data), num_enrollments)
